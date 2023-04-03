8730caa6b747ca3fd5955915547f6a67a105813b
obibby <obibby@student.42wolfsburg.de>
2023-04-03 23:20:20 +0100

    updated cgi

--- docs/www/pokemon/pokemon_question.py
+++ docs/www/pokemon/pokemon_question.py
@@ -8,7 +8,7 @@ choice = random.choice(choices)
 
 image = "images/" + choice + ".png"
 
-body = "<!DOCTYPE html>\n<head>\n<title>Who's that Pokemon?</title>\n</head>\n<body>\n"
+body = "<!DOCTYPE html>\n<head>\n<link href=/pokemon/pstyle.css rel=stylesheet type=text/css>\n<title>Who's that Pokemon?</title>\n</head>\n<body>\n"
 body += "<h1>Who's that Pokemon?</h1>\n"
 body += f"<img src={image} alt=\"{image}\">\n"
 body += "<form id=form action=/pokemon/pokemon_answer.py method=post enctype=multipart/form-data>\n"
