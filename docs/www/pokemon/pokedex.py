5e0bab93535b9dd3b641e7ef0425965bed1335f1
obibby <obibby@student.42wolfsburg.de>
2023-04-04 08:54:36 +0100

    add to CGI

---
+++ docs/www/pokemon/pokedex.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import cgi, requests
+
+form = cgi.FieldStorage()
+pokemon = form["pokemon"].file.read()
+pokemon = pokemon.lower()
+
+response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}/")
+
+body = "<!DOCTYPE html>\n<head>\n<link href=/pokemon/pstyle.css rel=stylesheet type=text/css>\n<title>Pokedex</title>\n</head>\n<body>\n"
+
+body += "<img src=/pokemon/images/pokeball.png alt=/images/pokeball>"
+
+if response.status_code == 200:
+	data = response.json()
+	body += f"<div>{pokemon.title()}</div>"
+	body += f"<div>Abilities:<br/>"
+	for ability in data["abilities"]:
+		body += f"- {ability['ability']['name'].capitalize()}<br/>"
+	body += "</div>"
+else:
+	body += f"<div>{pokemon.title()} not found.</div>"
+body += f"<div>Search again:</div>"
+body += "<div><form id=form action=/pokedex.py method=post enctype=multipart/form-data>"
+body += "<label for=pokemon>Enter a Pokemon:</label>"
+body += "<input id=pokemon type=text name=pokemon></input>"
+body += "<button id=button class=button type=submit value=Submit>Submit</button>"
+body += "</form></div>"
+body += "<form METHOD=GET ACTION=/>"
+body += "<button type=submit>Homepage</button>"
+body += "</form></body></html>"
+
+message = f"HTTP/1.1 200 OK\nContent-Length:{len(body)}\nContent-Type:text/html\r\n\r\n" + body
+print(message)
