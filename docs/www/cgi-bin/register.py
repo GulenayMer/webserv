f088f32d780b8e02bb1cba107c46ab3f3fe4c46d
jrocha <jrocha@student.42wolfsburg.de>
2023-04-05 08:56:00 +0000

    register.py

--- docs/www/cgi-bin/register.py
+++ docs/www/cgi-bin/register.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
 
-import cgi
+import cgi, os
 
 form = cgi.FieldStorage()
 
 username = form["username"].file.read()
 email = form["email"].file.read()
 password = form["password"].file.read()
+
+# check if db file exists, if not create it
+db_path = os.getcwd() + '/docs/www/user_db/users.csv
+if os.oath.exists(db_path):
+	# check if user exists
+
+	# if user does not exist create user
+else:
+	with open(file_path, "w") as db:
+        f.write("index,username,email,hash")
+        print("User Database was created.")
 
 body = "<html><head><link rel=stylesheet href=/utils/style.css><title>Data Submitted</title></head><body>"
 body += "<h1>Form Submitted.</h1>"
