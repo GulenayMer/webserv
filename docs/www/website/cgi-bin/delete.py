2428e42d0e3edb98d3d3ef3477b6077f071f5730
jrocha <jrocha@student.42wolfsburg.de>
2023-04-12 10:46:34 +0000

    delete users

--- docs/www/website/cgi-bin/delete.py
+++ docs/www/website/cgi-bin/delete.py
@@ -16,69 +16,94 @@ def is_json_file_valid(file_path):
         return True
 
 
-cookie = http.cookies.SimpleCookie(os.environ.get("HTTP_COOKIE"))
-
-# get user name from request
-if "session" in cookie:
-    # Extract the username from the session value
-    username = cookie["session"].value
-else:
-	username = None
-	error = True
-
-user = {
-	"username": username,
-}
-# find user in users_db
-db_path = os.environ["DB_PATH"]
-# File does not exist, create it with a list and add first user
 error = False
+# File does not exist, or file is empty
+# then user to delete does not exist, error is set to True
+db_path = os.environ["DB_PATH"]
 if not os.path.exists(db_path[:-10]):
 	error = True
 if not os.path.exists(db_path) or is_json_file_valid(db_path) == False:
 	error = True
-# db file exists, check user
+
+# Check if the cookie exists, if yes extract username,
+# else set error to True
+cookie = http.cookies.SimpleCookie(os.environ.get("HTTP_COOKIE"))
+if "session" in cookie:
+    # Extract the username from the session value
+    username = cookie["session"].value
 else:
+	cookie = http.cookies.SimpleCookie()
+	# set a value for the cookie
+	cookie['session'] = "logout"
+	cookie["session"]["path"] = "/"
+	cookie["session"]["max-age"] = 0
+	error = True
+
+# If error is still false proceed with user and cookie deletion
+if error == False:
+	# set a value for the cookie deletion
+	cookie["session"]["path"] = "/"
+	cookie["session"]["max-age"] = 0
+	user = {
+		"username": username,
+	}
+
+	# find user in users_db
 	user_exists = False
 	with open(db_path, "r", encoding='utf-8') as db:
 		users = json.load(db)
-	# check if user exists or email is taken
+	# check if user exists, if yes remove them
 	for entry in users:
 		if entry["username"] == user["username"]:
 			body = "Found user."
 			users.remove(entry)
 			user_exists = True
 			break
-	# delete user
+
+	# reset db without deleted user
 	if user_exists == True:
-		print(users)
-		
 		with open(db_path, 'w', encoding='utf-8') as db:
 			json.dump(users,db)
 
+	#  Prepare http response
+	body = "User deleted."
+	body = "<body>" + body + "</body>"
 
-# set a value for the cookie
-cookie["session"]["path"] = "/"
-cookie["session"]["max-age"] = 0
+	html = "<html>"
+	html += "<head>"
+	html += "<title>Redirecting...</title>"
+	html += "<meta http-equiv='refresh' content='0;url=/userDeleted.html'>"
+	html += "</head>"
+	html += body
+	html += "</html>"
 
-#  Prepare http response
-body = "User deleted."
-body = "<body>" + body + "</body>"
+	message = "HTTP/1.1 200 OK\r\n"
+	message += cookie.output()
+	message += "\n" # print a blank line to separate the headers from the body 
+	message += f"Content-length: {len(html)} \r\n"
+	message += "Content-type:text/html\r\n\r\n"
 
-html = "<html>"
-html += "<head>"
-html += "<title>Redirecting...</title>"
-html += "<meta http-equiv='refresh' content='0;url=/index.html'>"
-html += "</head>"
-html += body
-html += "</html>"
+	message += html
 
-message = "HTTP/1.1 200 OK\r\n"
-message += cookie.output()
-message += "\n" # print a blank line to separate the headers from the body 
-message += f"Content-length: {len(html)} \r\n"
-message += "Content-type:text/html\r\n\r\n"
+#  Prepare http response in case error is TRUE
+#  TODO where should it redirect to?
+else:
+	body = "User does not exist."
+	body = "<body>" + body + "</body>"
 
-message += html
+	html = "<html>"
+	html += "<head>"
+	html += "<title>Redirecting...</title>"
+	html += "<meta http-equiv='refresh' content='0;url=/index.html'>"
+	html += "</head>"
+	html += body
+	html += "</html>"
+
+	message = "HTTP/1.1 200 OK\r\n"
+	message += cookie.output()
+	message += "\n" # print a blank line to separate the headers from the body 
+	message += f"Content-length: {len(html)} \r\n"
+	message += "Content-type:text/html\r\n\r\n"
+	message += html
 
 print(message)
