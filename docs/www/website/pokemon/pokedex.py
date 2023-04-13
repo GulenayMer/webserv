18139d5c7624e26aa6dcc113aefb417ff1f4a000
obibby <obibby@student.42wolfsburg.de>
2023-04-13 11:52:11 +0000

    cgi update

--- docs/www/website/pokemon/pokedex.py
+++ docs/www/website/pokemon/pokedex.py
@@ -18,26 +18,26 @@ if response.status_code == 200:
 		evolution = response.json()
 		body += f"<style>div {{background-color: {evolution['color']['name']}}}</style>\n"
 		if evolution['evolves_from_species'] != None:
-			evolution_info += "<div>Evolves from:<br/>"
+			evolution_info += "<div id=d3>Evolves from:<br/>"
 			evolution_info += f"{evolution['evolves_from_species']['name'].capitalize()}<br/>"
 			evolution_info += "</div>"
 	body += "<title>Pokedex</title>\n</head>\n<body>\n"
 	sprite = data["sprites"]["front_default"]
 	body += f"<img src={sprite} alt=/images/pokeball width=200 height=200>"
-	body += f"<div>{pokemon.title()}</div>"
+	body += f"<div id=d3>{pokemon.title()}</div>"
 	body += "<div class=flex-container id=d3>"
-	body += "<div>Types:<br/>"
+	body += "<div id=d3>Types:<br/>"
 	for types in data["types"]:
 		body += f"- {types['type']['name'].capitalize()}<br/>"
 	body += "</div>"
-	body += "<div>Abilities:<br/>"
+	body += "<div id=d3>Abilities:<br/>"
 	for ability in data["abilities"]:
 		body += f"- {ability['ability']['name'].capitalize()}<br/>"
 	body += "</div>"
 	body += evolution_info
 else:
 	body += "<title>Pokedex</title>\n</head>\n<body>\n"
-	body += f"<div>{pokemon.title()} not found.</div>"
+	body += f"<div id=d3>{pokemon.title()} not found.</div>"
 body += "</div>"
 body += f"<div id=d1>Search again:</div>"
 body += "<div id=d2><form id=form action=/pokemon/pokedex.py method=post enctype=multipart/form-data>"
