067d138e09de187985aeee7ac87ddf121ef44423
libacchu <libacchu@students.42wolfsburg.de>
2023-04-22 17:28:17 +0000

    grim_reaper: waitpid in while loop

--- src/minilib.cpp
+++ src/minilib.cpp
@@ -178,11 +178,6 @@ void grim_reaper(int signum)
 		std::cout << GREEN << WEXITSTATUS(status) << RESET << std::endl;
 		pid = waitpid(-1, &status, WNOHANG);
 	}
-	// if (pid > 0)
-	// {
-	// 	exit_status.insert(std::map<int, int>::value_type(pid, WEXITSTATUS(status)));
-	// 	std::cout << GREEN << WEXITSTATUS(status) << RESET << std::endl;
-	// }
 }
 
 std::string clean_response_path(std::string response_path)
